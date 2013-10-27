e1a5bb56e60da6cdae29521e6c83cd3292f2ac44
Julian Churchill <chewielouie03@gmail.com>
2013-10-27 20:39:41 +0000

    Added first Presenter.render test and lots of other suggested tests commented out

--- www/spec/textAdventurePresenterSpec.js
+++ www/spec/textAdventurePresenterSpec.js
@@ -1,10 +1,15 @@
 
-function Presenter(model) {
+function Presenter(model, view) {
     this.model = model;
+    this.view = view;
 }
 
 Presenter.prototype.exitTriggered = function(exitid) {
     this.model.exitTriggered( exitid );
+};
+
+Presenter.prototype.render = function() {
+    this.view.onDescriptionChanged( this.model.currentLocationDescription() );
 };
 
 function Model() {
@@ -13,12 +18,19 @@ function Model() {
 Model.prototype.exitTriggered = function(exitid) {
 };
 
+Model.prototype.currentLocationDescription = function() {
+    return '';
+};
+
 describe('TextAdventurePresenter', function() {
+    beforeEach(function() {
+        model = new Model();
+        presenter = new Presenter( model, view );
+    });
+
     describe('on exit action', function() {
         it('should tell model to use an exit', function() {
-            model = new Model();
             spyOn( model, 'exitTriggered' );
-            presenter = new Presenter( model );
 
             presenter.exitTriggered( 'exitid' );
 
@@ -26,14 +38,53 @@ describe('TextAdventurePresenter', function() {
         });
     });
 
-    //describe('on render', function() {
-        //it('should tell the view what the current location description is', function() {
-        //});
+    describe('on render', function() {
+        it('should tell the view what the current location description is', function() {
+            spyOn( model, 'currentLocationDescription' ).andReturn( 'description' );
+            spyOn( view, 'onDescriptionChanged' );
+
+            presenter.render();
+
+            expect(view.onDescriptionChanged).toHaveBeenCalledWith( 'description' );
+        });
         //it('should tell the view what the current location exits are', function() {
         //});
         //it('should tell the view what the current location items are', function() {
         //});
         //it('should tell the view what the current available actions are', function() {
+        //});
+    });
+
+    //describe('on event from model', function() {
+        //describe('current location changed', function() {
+            //it('should tell the view what the current location description is', function() {
+            //});
+            //it('should tell the view what the current location exits are', function() {
+            //});
+            //it('should tell the view what the current location items are', function() {
+            //});
+            //it('should tell the view what the current available actions are', function() {
+            //});
+        //});
+
+        //describe('current location description changed', function() {
+            //it('should tell the view what the current location description is', function() {
+            //});
+        //});
+
+        //describe('current location exits changed', function() {
+            //it('should tell the view what the current location exits are', function() {
+            //});
+        //});
+
+        //describe('current location items changed', function() {
+            //it('should tell the view what the current location items are', function() {
+            //});
+        //});
+
+        //describe('current available actions changed', function() {
+            //it('should tell the view what the current available actions are', function() {
+            //});
         //});
     //});
 });
