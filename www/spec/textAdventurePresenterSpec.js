4ea8ac9945e055917c8d16506cc34df2ccbbdefb
Julian Churchill <chewielouie03@gmail.com>
2013-10-28 20:33:42 +0000

    Added more tests for presenter interaction with view during render

--- www/spec/textAdventurePresenterSpec.js
+++ www/spec/textAdventurePresenterSpec.js
@@ -9,7 +9,10 @@ Presenter.prototype.exitTriggered = function(exitid) {
 };
 
 Presenter.prototype.render = function() {
-    this.view.onDescriptionChanged( this.model.currentLocationDescription() );
+    this.view.onDescriptionChanged( this.model.currentDescription() );
+    this.view.onExitsChanged( this.model.currentExits() );
+    this.view.onItemsChanged( this.model.currentItems() );
+    this.view.onActionsChanged( this.model.currentActions() );
 };
 
 function Model() {
@@ -18,14 +21,30 @@ function Model() {
 Model.prototype.exitTriggered = function(exitid) {
 };
 
-Model.prototype.currentLocationDescription = function() {
+Model.prototype.currentDescription = function() {
     return '';
+};
+
+Model.prototype.currentExits = function() {
+    return [];
+};
+
+Model.prototype.currentItems = function() {
+    return [];
+};
+
+Model.prototype.currentActions = function() {
+    return [];
 };
 
 describe('TextAdventurePresenter', function() {
     beforeEach(function() {
         model = new Model();
-        presenter = new Presenter( model, view );
+        my_view = jasmine.createSpyObj('view', ['onDescriptionChanged',
+                                                'onExitsChanged',
+                                                'onItemsChanged',
+                                                'onActionsChanged']);
+        presenter = new Presenter( model, my_view );
     });
 
     describe('on exit action', function() {
@@ -40,19 +59,39 @@ describe('TextAdventurePresenter', function() {
 
     describe('on render', function() {
         it('should tell the view what the current location description is', function() {
-            spyOn( model, 'currentLocationDescription' ).andReturn( 'description' );
-            spyOn( view, 'onDescriptionChanged' );
+            spyOn( model, 'currentDescription' ).andReturn( 'description' );
 
             presenter.render();
 
-            expect(view.onDescriptionChanged).toHaveBeenCalledWith( 'description' );
+            expect(my_view.onDescriptionChanged).toHaveBeenCalledWith( 'description' );
         });
-        //it('should tell the view what the current location exits are', function() {
-        //});
-        //it('should tell the view what the current location items are', function() {
-        //});
-        //it('should tell the view what the current available actions are', function() {
-        //});
+
+        it('should tell the view what the current exits are', function() {
+            var exits = [{ label: 'exit1', id: 'exit1' }];
+            spyOn( model, 'currentExits' ).andReturn( exits );
+
+            presenter.render();
+
+            expect(my_view.onExitsChanged).toHaveBeenCalledWith( exits );
+        });
+
+        it('should tell the view what the current items are', function() {
+            var items = [{ label: 'item1', id: 'item1' }];
+            spyOn( model, 'currentItems' ).andReturn( items );
+
+            presenter.render();
+
+            expect(my_view.onItemsChanged).toHaveBeenCalledWith( items );
+        });
+
+        it('should tell the view what the current available actions are', function() {
+            var actions = [{ id: 'action1' }];
+            spyOn( model, 'currentActions' ).andReturn( actions );
+
+            presenter.render();
+
+            expect(my_view.onActionsChanged).toHaveBeenCalledWith( actions );
+        });
     });
 
     //describe('on event from model', function() {
